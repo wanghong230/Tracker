e1e476f9a98751ef88d10a50f6beb5985bbd5110
Hong Wang <hong.wang@sv.cmu.edu>
2013-09-26 15:26:59 -0400

    Add start and stop buttons and bind them to the listener. When click button could log some message. Add a skelton of trackerService.

--- src/com/example/tracker/MainActivity.java
+++ src/com/example/tracker/MainActivity.java
@@ -2,18 +2,32 @@ package com.example.tracker;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
-	private String TAG = this.getClass().getSimpleName();	
+	private String TAG = this.getClass().getSimpleName();
+	private Intent trackerService = null;
+	private Button startButton = null;
+	private Button stopButton = null;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     	Log.i(TAG, "onCreat() is called"); 
         super.onCreate(savedInstanceState);
+        trackerService = new Intent(this, TrackerService.class);
         setContentView(R.layout.activity_main);
+        
+        /** Bind the button listener here */
+        startButton = (Button)findViewById(R.id.startbutton);
+        stopButton = (Button)findViewById(R.id.stopbutton);
+        startButton.setOnClickListener(this);
+        stopButton.setOnClickListener(this);
     }
 
 
@@ -23,5 +37,27 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		switch(arg0.getId()) {
+			case R.id.startbutton:
+				Log.i(TAG, "startbutton clicked.");
+				break;
+			case R.id.stopbutton:
+				Log.i(TAG, "stopbuttion clicked.");
+				break;
+			default:
+					break;
+		}		
+	}
 }
---
+++ src/com/example/tracker/TrackerService.java
@@ -0,0 +1,27 @@
+package com.example.tracker;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TrackerService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+	
+}
